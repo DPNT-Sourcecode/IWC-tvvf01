5b59f586c99050a1d9584a446b501285bf597448
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-17 22:20:55 +0000

    Sat Jan 17 22:20:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution_legacy.py
+++ test/solution_tests/IWC/test_queue_solution_legacy.py
@@ -1,20 +1,31 @@
 import pytest
 from datetime import date
 from solutions.IWC.queue_solution_legacy import Queue, COMPANIES_HOUSE_PROVIDER, CREDIT_CHECK_PROVIDER, BANK_STATEMENTS_PROVIDER, Priority
+from solutions.IWC.task_types import TaskSubmission
+
 
 @pytest.fixture
 def queue():
     return Queue()
 
+
 def test_age(queue):
     assert queue.age == 0
+
 
 def test_size_empty_queue(queue):
     assert queue.size == 0
 
+
 def test_size_busy_queue(queue):
-    queue.enqueue({ "provider": CREDIT_CHECK_PROVIDER, "user_id": 123, "timestamp": date(2026, 1, 15) })
+    credit_check_task = TaskSubmission(provider=CREDIT_CHECK_PROVIDER, user_id=123, timestamp=date(2026, 1, 15))
+    bank_statement_task = TaskSubmission(provider=BANK_STATEMENTS_PROVIDER, user_id=123, timestamp=date(2026, 1, 15))
+    queue.enqueue(credit_check_task)
+    queue.enqueue(bank_statement_task)
+    queue.dequeue()
 
     assert queue.size == 1
+
+
 
 
