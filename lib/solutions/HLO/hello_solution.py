35ec70c98321ed4c46ef97c5b257d4e8e5fc9ca4
sbx_user1051 <sbx_user1051@169.254.39.153>
2026-01-17 21:45:26 +0000

    Sat Jan 17 21:45:26 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,6 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name):
-        raise NotImplementedError()
+    def hello(self, friend_name: str) -> str:
+        return f"Hello, {friend_name}"
+
---
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -0,0 +1,6 @@
+from solutions.HLO.hello_solution import HelloSolution
+
+
+class TestSum():
+    def test_hello(self):
+        assert HelloSolution.hello(self, "James") == "Hello, James"
