c10ad05e130a2ac06a8517cbc2d94ce9ce3ec734
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-17 22:14:55 +0000

    Sat Jan 17 22:14:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution_legacy.py
+++ test/solution_tests/IWC/test_queue_solution_legacy.py
@@ -1,1 +1,16 @@
-from solutions.IWC.queue_solution_legacy import QueueSolutionEntrypoint
+import pytest
+from solutions.IWC.queue_solution_legacy import Queue
+
+@pytest.fixture
+def queue():
+    return Queue()
+
+def test_age(queue):
+    assert queue.age == 0
+
+def test_size_empty_queue(queue):
+    assert queue.size == 0
+
+def test_size_busy_queue(queue):
+    queue.enqueue
+
