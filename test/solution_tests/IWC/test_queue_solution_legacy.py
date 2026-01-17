2c395857c06752c8656ed0b06c890e29cd999584
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-17 22:17:55 +0000

    Sat Jan 17 22:17:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution_legacy.py
+++ test/solution_tests/IWC/test_queue_solution_legacy.py
@@ -1,5 +1,6 @@
 import pytest
-from solutions.IWC.queue_solution_legacy import Queue
+from datetime import date
+from solutions.IWC.queue_solution_legacy import Queue, COMPANIES_HOUSE_PROVIDER, CREDIT_CHECK_PROVIDER, BANK_STATEMENTS_PROVIDER, Priority
 
 @pytest.fixture
 def queue():
@@ -12,5 +13,8 @@ def test_size_empty_queue(queue):
     assert queue.size == 0
 
 def test_size_busy_queue(queue):
-    queue.enqueue
+    queue.enqueue({ "provider": CREDIT_CHECK_PROVIDER, "user_id": 123, "timestamp": date(2026, 1, 15) })
+
+    assert queue.size == 1
+
 
