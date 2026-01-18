95ae3792b47714f62d05edb43595c84712f616f0
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-18 03:07:33 +0000

    Sun Jan 18 03:07:33 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -77,7 +77,7 @@ class Queue:
         
         task_internal_age = int((self._newest_task_timestamp - self._timestamp_for_task(task)).total_seconds())
         is_task_internal_age_within_limits = task_internal_age < 300
-        # is_task_internal_age_within_limits = True
+
         return is_deprioritised_provider and is_task_internal_age_within_limits
 
     @staticmethod
@@ -305,3 +305,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
