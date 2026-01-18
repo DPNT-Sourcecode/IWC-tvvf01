4276dc8a02a2307749b7086637e6c18ab7e8e4e5
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-18 04:17:05 +0000

    Sun Jan 18 04:17:05 UTC 2026

--- test/solution_tests/IWC/test_queue_solution_legacy.py
+++ test/solution_tests/IWC/test_queue_solution_legacy.py
@@ -167,20 +167,21 @@ def test_reprioritise_bank_statement_when_internal_age_five_mins_or_more(queue):
 
 
 def test_reprioritise_bank_statement_when_internal_age_five_mins_or_more_multiuser(queue):
-    bank_statements_task_u1 = TaskSubmission(provider=BANK_STATEMENTS_PROVIDER.name, user_id=123, timestamp="2025-10-20 12:01:00", metadata={ "priority": Priority.NORMAL })
-    id_verif_task_u2 = TaskSubmission(provider=ID_VERIFICATION_PROVIDER.name, user_id=234, timestamp="2025-10-20 12:06:00", metadata={ "priority": Priority.NORMAL })
+    bank_statements_task_u1 = TaskSubmission(provider=BANK_STATEMENTS_PROVIDER.name, user_id=123, timestamp="2025-10-20 12:00:00", metadata={ "priority": Priority.NORMAL })
+    ch_u2 = TaskSubmission(provider=COMPANIES_HOUSE_PROVIDER.name, user_id=234, timestamp="2025-10-20 12:01:00", metadata={ "priority": Priority.NORMAL })
+    id_v_u2 = TaskSubmission(provider=ID_VERIFICATION_PROVIDER.name, user_id=234, timestamp="2025-10-20 12:06:00", metadata={ "priority": Priority.NORMAL })
     bank_statements_u2 = TaskSubmission(provider=BANK_STATEMENTS_PROVIDER.name, user_id=234, timestamp="2025-10-20 12:07:00", metadata={ "priority": Priority.NORMAL })
-    companies_house_u2 = TaskSubmission(provider=COMPANIES_HOUSE_PROVIDER.name, user_id=234, timestamp="2025-10-20 12:08:00", metadata={ "priority": Priority.NORMAL })
 
-    queue.enqueue(companies_house_u2)
     queue.enqueue(bank_statements_task_u1)
-    queue.enqueue(id_verif_task_u2)
+    queue.enqueue(ch_u2)
+    queue.enqueue(id_v_u2)
     queue.enqueue(bank_statements_u2)
-    queue.enqueue(companies_house_u1)
-    queue.enqueue(id_verification_u1)
 
     assert queue.dequeue().provider == BANK_STATEMENTS_PROVIDER.name
     assert queue.dequeue().provider == COMPANIES_HOUSE_PROVIDER.name
+    assert queue.dequeue().provider == ID_VERIFICATION_PROVIDER.name
+    assert queue.dequeue().provider == BANK_STATEMENTS_PROVIDER.name
+
 
 
 
