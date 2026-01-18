d483b7eef0fbb5ef08947479b0bc4b9277a4e0cb
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-18 03:34:33 +0000

    Sun Jan 18 03:34:33 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -183,6 +183,7 @@ class Queue:
             if self._should_deprioritise_task(task) and not self._should_reprioritise_deprioritised_task(task):
                 metadata["complexity_weighting"] = 2
 
+            print(f"{task.provider=}, {self._should_reprioritise_deprioritised_task(task)=}")
         queued_tasks.sort(
             key=lambda i: (
                 self._priority_for_task(i),
@@ -309,10 +310,3 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
-
-
-
-
-
-
-
--- test/solution_tests/IWC/test_queue_solution_legacy.py
+++ test/solution_tests/IWC/test_queue_solution_legacy.py
@@ -164,8 +164,7 @@ def test_reprioritise_bank_statement_when_internal_age_five_mins_or_more(queue):
     queue.enqueue(bank_statements_task)
     queue.enqueue(companies_house_task)
 
-    assert queue.dequeue().provider == ID_VERIFICATION_PROVIDER.name
     assert queue.dequeue().provider == BANK_STATEMENTS_PROVIDER.name
-
+    assert queue.dequeue().provider == ID_VERIFICATION_PROVIDER.name
 
 
