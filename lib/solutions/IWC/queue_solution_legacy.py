e560fe5267ad1f8527cc75f237485d34ded51f6f
sbx_user1051 <sbx_user1051@169.254.28.89>
2026-01-18 03:37:33 +0000

    Sun Jan 18 03:37:33 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -79,6 +79,7 @@ class Queue:
 
     def _should_reprioritise_deprioritised_task(self, task: TaskSubmission) -> bool:
         task_internal_age = int((self._newest_task_timestamp - self._timestamp_for_task(task)).total_seconds())
+        print(f"{task_internal_age=}, {self._newest_task_timestamp=}, {self._timestamp_for_task(task)=}")
         is_task_internal_age_above_limit = task_internal_age >= 300
 
         return self._should_deprioritise_task(task) and is_task_internal_age_above_limit
@@ -184,6 +185,13 @@ class Queue:
                 metadata["complexity_weighting"] = 2
 
             print(f"{task.provider=}, {self._should_reprioritise_deprioritised_task(task)=}")
+
+        for i in queued_tasks:
+            print(self._priority_for_task(i),
+                self._earliest_group_timestamp_for_task(i),
+                self._complexity_weighting_for_task(i),
+                self._timestamp_for_task(i),
+                self._should_reprioritise_deprioritised_task(task) == False)
         queued_tasks.sort(
             key=lambda i: (
                 self._priority_for_task(i),
@@ -310,3 +318,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
